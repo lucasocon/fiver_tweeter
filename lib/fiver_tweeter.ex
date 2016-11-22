8fcb8675a1de670bc5cd0c8b3174feb306108d20
lucasocon <lucas_masterclas@hotmail.com>
2016-11-22 19:55:43 -0300

    Add supervisor and GenServer, set scheduler to send tweets

--- lib/fiver_tweeter.ex
+++ lib/fiver_tweeter.ex
@@ -9,12 +9,15 @@ defmodule FiverTweeter do
     # Define workers and child supervisors to be supervised
     children = [
       # Starts a worker by calling: FiverTweeter.Worker.start_link(arg1, arg2, arg3)
-      # worker(FiverTweeter.Worker, [arg1, arg2, arg3]),
+      worker(FiverTweeter.TweetServer, []),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: FiverTweeter.Supervisor]
-    Supervisor.start_link(children, opts)
+    process = Supervisor.start_link(children, opts)
+    FiverTweeter.Scheduler.schedule_file("* * * * *", Path.join("#{:code.priv_dir(:fiver_tweeter)}", "sample.txt"))
+
+    process
   end
 end
--- lib/fiver_tweeter/scheduler.ex
+++ lib/fiver_tweeter/scheduler.ex
@@ -1,5 +1,6 @@
 defmodule FiverTweeter.Scheduler do
-  def schedule_file(schedule, file) do
-    Quantum.add_job(schedule, fn -> IO.puts FiverTweeter.FileReader.get_string_to_tweet(file) end)
+  def schedule_file(schedule, filepath) do
+    Quantum.add_job(schedule, fn -> FiverTweeter.FileReader.get_string_to_tweet(filepath)
+    |> FiverTweeter.TweetServer.tweet end)
   end
 end
